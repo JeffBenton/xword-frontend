69d673490749bfd313bec2853caf7896240a39ea
alex <alex@frogrock.net>
2015-12-02 11:20:23 -0800

    add click handler to clue

--- src/components/Crossword/CrosswordClue.js
+++ src/components/Crossword/CrosswordClue.js
@@ -3,12 +3,31 @@
  */
 
 import React from 'react';
+import Clue from './../../objects/clue.js';
 
 class CrosswordClue extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.onClick = this.onClick.bind(this);
+    }
+
+    onClick() {
+        this.props.onClick(this.props.clue);
+    }
+
     render() {
-        return (<div><b>{this.props.clue.number}</b> {this.props.clue.text}</div>);
+        return (<div onClick={this.onClick}><b>{this.props.clue.number}</b> {this.props.clue.text}</div>);
     }
 }
+
+CrosswordClue.propTypes = {
+    clue: React.PropTypes.instanceOf(Clue).isRequired,
+    isEditing: React.PropTypes.bool
+};
+
+CrosswordClue.defaultProps = {
+    isEditing: false
+};
 
 module.exports = CrosswordClue;
--- src/objects/clue.js
+++ src/objects/clue.js
@@ -4,7 +4,7 @@ class Clue {
     constructor(direction, number) {
         this.direction = direction;
         this.number = number;
-        this.text = number + " " + direction;
+        this.text = "";
         this.isSelected = false;
     }
 }
