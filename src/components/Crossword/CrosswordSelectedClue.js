cfd362ebb69cddb93184492c5dd8e69e4b39fda6
alex <alex@frogrock.net>
2015-12-27 17:31:16 -0500

    keep the selected clue on screen

--- src/components/Crossword/CrosswordSelectedClue.js
+++ src/components/Crossword/CrosswordSelectedClue.js
@@ -4,20 +4,54 @@
  */
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './CrosswordSelectedClue.css';
 
 class CrosswordSelectedClue extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleWindowScroll = this.handleWindowScroll.bind(this);
+        this.state = {
+            isFloating: false
+        };
+    }
+
+    componentDidMount() {
+        window.addEventListener("scroll", this.handleWindowScroll);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleWindowScroll);
+    }
+
+    handleWindowScroll() {
+        let rect = ReactDOM.findDOMNode(this).getBoundingClientRect();
+        if (!this.state.isFloating && (rect.bottom - (rect.height/2)  <= 0)) {
+            this.setState({isFloating: true});
+        } else if (this.state.isFloating && (rect.top + (rect.height/2) >= 0)) {
+            this.setState({isFloating: false});
+        }
+    }
 
     render() {
         if (this.props.clue !== null && this.props.clue.number && this.props.clue.direction) {
             let clueName = (
                 <span className="clue-name">{this.props.clue.number + " " + this.props.clue.direction + " |"}</span>);
             let clueText = (<span className="clue-text">{this.props.clue.text ? this.props.clue.text : <i>(no clue text)</i>}</span>);
-            return (
-                <div className={"crossword-selected-clue-container active"}>
-                    {clueName}
-                    {clueText}
+            let element = <div className={"crossword-selected-clue-container active" + (this.state.isFloating ? " ghost" : "")}>
+                {clueName}
+                {clueText}
+            </div>;
+            if (this.state.isFloating) {
+                let floatbar = <div className="crossword-selected-clue-container-floating">{clueName}{clueText}</div>;
+                return (<div>
+                    {floatbar}
+                    {element}
                 </div>);
+            } else {
+                return element;
+            }
         } else {
             return (<div className={"crossword-selected-clue-container"}></div>);
         }
