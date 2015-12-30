1e9604323eccd09414f998d0f295ca00d60eb8e3
alex <alex@frogrock.net>
2015-12-30 06:45:31 -0500

    small fix to crossword title

--- src/components/Crossword/CrosswordTitle.js
+++ src/components/Crossword/CrosswordTitle.js
@@ -16,13 +16,13 @@ class CrosswordTitle extends React.Component {
     render() {
         return (<div className="crossword-title-container">
             <div className="title">
-                <span>{this.props.title || this.DEFAULT_TITLE}</span>
+                <span>{this.props.data.title || this.DEFAULT_TITLE}</span>
             </div>
             {this.props.data ? (<div className="metadata">
                 {(this.props.data.author) ?
                     (<div><span className="field">created by</span><span className="value">{this.props.data.author}</span></div>) : ""}
                 {(this.props.data.editor) ?
-                    (<div><span className="field">edited by</span><span className="value">{this.props.data.author}</span></div>) : ""}
+                    (<div><span className="field">edited by</span><span className="value">{this.props.data.editor}</span></div>) : ""}
             </div>) : ""}
         </div>);
     }
